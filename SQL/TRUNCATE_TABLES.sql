4e15a4cb274b0ee554203be81a5701f71e0d452b
carlossalazar7 <carlossalazar2228@gmail.com>
2022-03-17 15:39:04 -0600

    truncate tables stg

---
+++ SQL/TRUNCATE_TABLES.sql
@@ -0,0 +1,21 @@
+
+truncate open_cashier_stg;
+select * from open_cashier_stg;
+
+truncate close_cashier_stg;
+select * from close_cashier_stg;
+
+truncate customer_stg;
+select * from customer_stg;
+
+truncate ITEM_STG;
+select * from ITEM_STG;
+
+truncate tran_head_stg;
+select * from tran_head_stg;
+
+truncate tran_item_stg;
+select * from tran_item_stg;
+
+truncate tran_tender_stg;
+select * from tran_item_stg;
