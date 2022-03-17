c0bcd746e9a306117f71ffe1f352fbba0243f989
carlossalazar7 <carlossalazar2228@gmail.com>
2022-03-16 18:36:50 -0600

    cambios en ejecuciòn de bat

--- bat/main.bat
+++ bat/main.bat
@@ -1,15 +1,15 @@
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\open_cashier.kjb /level Basic
+start /b C:\apps\pos\bat\open_cashier_get.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\close_cashier_bajada.kjb /level Basic
+start /b C:\apps\pos\bat\close_cashier_get.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\etlCustomers.kjb /level Basic
+start /b C:\apps\pos\bat\customer_get.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\items_stg_job.kjb /level Basic
+start /b C:\apps\pos\bat\item_stg_get.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\jobbajada.kjb /level Basic
+start /b C:\apps\pos\bat\tran_head_get.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\tran_item_stg(bajada).kjb /level Basic
+start /b C:\apps\pos\bat\tran_item_get.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\job_bajada_tran_tender_stg.kjb /level Basic
+start /b C:\apps\pos\bat\tran_tender_get.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\file_transfer_main.kjb /level Basic
+start /b C:\apps\pos\bat\file_transfer.bat
--- bat/main_hist.bat
+++ bat/main_hist.bat
@@ -1,15 +1,17 @@
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\hist\open_cashier_stg_his.kjb /level Detailed
+start /b C:\apps\pos\bat\open_cashier_his.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\hist\close_cashier_stg_his.kjb /level Basic
+start /b C:\apps\pos\bat\close_cashier_hist.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\hist\customer_stg_hist_bajada.kjb /level Basic
+start /b C:\apps\pos\bat\customer_hist.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\hist\items_stg_his.kjb /level Basic
+start /b C:\apps\pos\bat\item_stg_hist.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\hist\tran_head_hist.kjb /level Basic
+start /b C:\apps\pos\bat\tran_head_hist.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\hist\tran_item_stg_hist(bajada).kjb /level Basic
+start /b C:\apps\pos\bat\tran_item_hist.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\hist\tran_tender_stg.kjb /level Basic
+start /b C:\apps\pos\bat\tran_tender_hist.bat
 
-"C:\Program Files\Java\jdk1.8.0_231\bin\java.exe" -Xmx512m -Djava.library.path=C:\spoon-5.0.1\data-integration\libswt\win32 -DKETTLE_HOME=C:\apps\pos -jar C:\spoon-5.0.1\data-integration\launcher\pentaho-application-launcher-5.0.1-stable.jar -main org.pentaho.di.kitchen.Kitchen /file:C:\apps\pos\etl\bajada\file_transfer_main.kjb /level Basic
+start /b C:\apps\pos\bat\file_transfer.bat
+
+shutdown \s \t 20
